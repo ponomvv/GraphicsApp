1f284d96f5eb9575f3944abda0677442e078b07f
ponomvv <ponomvv@gmail.com>
2021-06-27 18:48:34 +0300

    Кисть простая SolidColorBrush.

--- GraphicsApp/MainPage.xaml.cs
+++ GraphicsApp/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI;
 
 // Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
 
@@ -24,7 +25,15 @@ namespace GraphicsApp
     {
         public MainPage()
         {
+            //Цвет кнопки с помощью кода
             this.InitializeComponent();
+            Button myButton2 = new Button();
+            Color redColor = Color.FromArgb(255, 255, 0, 0);
+            SolidColorBrush buttonBrush = new SolidColorBrush(redColor);
+            myButton2.Content = "HELLO2";
+            myButton2.Margin = new Thickness(10);
+            myButton2.Background = buttonBrush;
+            stackPanel.Children.Add(myButton2);
         }
     }
 }
